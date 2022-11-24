71d73bcfb9c89a301c5c4415dfbaf606137bda7a
naba93980 <72063612+naba93980@users.noreply.github.com>
2022-11-25 01:15:51 +0530

    added another case of optional chaining

--- CodeWithMosh/13.OptionalChaining/index.js
+++ CodeWithMosh/13.OptionalChaining/index.js
@@ -1,6 +1,14 @@
+var _a;
 function getCustomer(id) {
     return id === 0 ? null : { birthday: new Date() };
 }
 var customer = getCustomer(0);
+
 // optional property access operator
-console.log(customer === null || customer === void 0 ? void 0 : customer.birthday);
+console.log((_a = customer === null || customer === void 0 ? void 0 : customer.birthday) === null || _a === void 0 ? void 0 : _a.getFullYear);
+
+// a=10 evaluates to 10, basically console.log(a=10) will print 10
+
+// let log: any = (message: string) => console.log(message);
+var log = null;
+log === null || log === void 0 ? void 0 : log('a');
--- CodeWithMosh/13.OptionalChaining/index.ts
+++ CodeWithMosh/13.OptionalChaining/index.ts
@@ -1,7 +1,6 @@
 type Customer = {
     birthday: Date;
 }
-
 function getCustomer(id: number): Customer | null | undefined{
     return id === 0 ? null : { birthday: new Date() };
 }
@@ -9,4 +8,12 @@ function getCustomer(id: number): Customer | null | undefined{
 let customer = getCustomer(0);
 
 // optional property access operator
-console.log(customer?.birthday);
+console.log(customer?.birthday?.getFullYear);
+
+// a=10 evaluates to 10, basically console.log(a=10) will print 10
+
+// let log: any = (message: string) => console.log(message);
+let log: any = null
+log?.('a');
+
+
