b200694f76594a7023578321c005c9dd3d3bd25b
naba93980 <72063612+naba93980@users.noreply.github.com>
2022-12-13 02:00:51 +0530

    generic type extending other type

--- CodeWithMosh/17.Generics/basics/index.js
+++ CodeWithMosh/17.Generics/basics/index.js
@@ -9,3 +9,21 @@ var bigBottle = {
 };
 var theBottle = identity(bigBottle);
 console.log(theBottle);
+function anotherFunction(valOne, valTwo) {
+    return {
+        valOne: valOne,
+        valTwo: valTwo
+    };
+}
+var resultOne = anotherFunction(false, {
+    connection: "connect",
+    namKyaHai: "nabajyoti Hai",
+    passwordBhiBolDe: "nhi Bolunga"
+});
+var resultTwo = anotherFunction(true, {
+    connection: "connect",
+    username: "naba",
+    password: "pass"
+});
+console.log(resultOne);
+console.log(resultTwo);
--- CodeWithMosh/17.Generics/basics/index.ts
+++ CodeWithMosh/17.Generics/basics/index.ts
@@ -17,3 +17,38 @@ const bigBottle: Bottle = {
 
 const theBottle = identity<Bottle>(bigBottle)
 console.log(theBottle);
+
+
+interface Database {
+    connection: string,
+}
+interface DatabaseAdmin extends Database{
+    username: string,
+    password: string
+}
+
+function anotherFunction<T, U extends Database>(valOne: T, valTwo: U): object {
+    return {
+        valOne,
+        valTwo
+    };
+}
+
+let resultOne: object = anotherFunction<boolean, {
+    connection: string,
+    namKyaHai: string,
+    passwordBhiBolDe: string
+}>(false, {
+    connection: "connect",
+    namKyaHai: "nabajyoti Hai",
+    passwordBhiBolDe: "nhi Bolunga"
+});
+
+let resultTwo=anotherFunction<boolean, DatabaseAdmin>(true, {
+    connection: "connect",
+    username: "naba",
+    password: "pass"
+});
+
+console.log(resultOne);
+console.log(resultTwo);
